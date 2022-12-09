ff9a33a4656bd10b1ca3feff6c264a75d98422fd
Saif-Ul-llah <saifhammad411@gmail.com>
2022-12-09 19:30:26 +0500

    some code

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 // const express = require('express')
 import express from "express";
+import path from 'path';
 
 console.log("i am a sever machice or software");
 const app = express()
@@ -11,6 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World! saif is here'+ new Date().toString());
 
 })
+
+const __dirname =path.resolve();
+app.use('/static', express.static(path.join(__dirname, './web/build')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
